e5301438bccb85f6d9d625ca3204bd4e4a517723
gomba66 <danielalejo66@hotmail.com>
2020-04-29 22:04:26 -0500

    I deleted some innnecesary importations

--- MiscuaApp/App.js
+++ MiscuaApp/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import { LoginMiscua } from './components/LoginMiscua';
 import { howto } from './components/howto';
 import { main } from './components/main';
@@ -20,7 +18,6 @@ import { register } from './components/register';
 import { AuthContext } from "./context";
 import { passwordRequest } from './components/passwordRequest';
 import {
-  SignIn,
   Splash
 } from "./Screens";
 
